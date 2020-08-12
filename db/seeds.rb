6e76688b2d0b88ba9856fb2e61edb408bd77fa9b
Linda Walczak lind.walczak@gmail.com <lind.walczak@gmail.com>
2020-08-12 15:32:22 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Restaurants being destroyed'
+Restaurant.destroy_all
+
+10.times do
+  name = Faker::Hipster.word
+  address = Faker::Address.street_address
+  category = ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+
+  Restaurant.create(name: name, address: address, category: category)
+  puts "Restaurant #{name} created"
+end
+
+puts 'All restaurants created'
